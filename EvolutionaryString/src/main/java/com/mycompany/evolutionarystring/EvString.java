1337a427e794ca060f1523ae43ed9ceaebf8c606
sb00020 <sb00020@googlemail.com>
2016-12-15 14:45:14 +0000

    s

--- EvolutionaryString/src/main/java/com/mycompany/evolutionarystring/EvString.java
+++ EvolutionaryString/src/main/java/com/mycompany/evolutionarystring/EvString.java
@@ -17,42 +17,102 @@ import java.util.Random;
 public class EvString {
 
     Random rnd = new Random(System.currentTimeMillis());
+    char[] chars;
 
     public void run() {
         String target = "Hello, World!";
         int targetLength = target.length();
 
-        char[] chars = target.toCharArray();
+        chars = target.toCharArray();
 
         ArrayList<char[]> parents = createInitialParents(8, targetLength);
 
         ArrayList<StringAndScore> scores = new ArrayList<>();
-        
+
         for (int i = 0; i < parents.size(); i++) {
-            
+
             String s = new String(parents.get(i));
             int score = this.getScore(target.toCharArray(), parents.get(i));
-            
+
             scores.add(new StringAndScore(s, score));
 
         }
-        
-Collections.sort(scores, new ScoreComparator());
+
+        Collections.sort(scores, new ScoreComparator());
         for (int i = 0; i < parents.size(); i++) {
-            
+
             System.out.println(scores.get(i).rndString + scores.get(i).score);
 
         }
 
+        for (int l = 0; l < 8000; l++) {
+            ArrayList<StringAndScore> children = produceChildrenFromFittestParents(scores.get(0), scores.get(1), scores.size());
+            Collections.sort(children, new ScoreComparator());
+
+            for (int i = 0; i < children.size(); i++) {
+
+                System.out.println(children.get(i).rndString + "  " + children.get(i).score);
+
+            }
+
+            scores = children;
+            System.out.println("");
+            System.out.println("");
+        }
+
+    }
+
+    private ArrayList<StringAndScore> produceChildrenFromFittestParents(StringAndScore p1, StringAndScore p2, int length) {
+
+        ArrayList<StringAndScore> childrenss = new ArrayList<>();
+
+        System.out.println("p1: " + p1.rndString);
+        System.out.println("p2: " + p2.rndString);
+
+        for (int j = 0; j < length; j++) {
+
+            char[] child = p1.rndString.toCharArray();
+
+            int p2Dom = rnd.nextInt(length-1);
+
+            for (int i = 0; i < p2Dom; i++) {
+
+                int c1 = rnd.nextInt(p1.getRndString().length());
+                int c2 = rnd.nextInt(p1.getRndString().length());
+
+                child[c1] = p2.getRndString().toCharArray()[c2];
+
+            }
+            
+            child = mutateChild(child);
+            
+            childrenss.add(new StringAndScore(new String(child), StringAndScore.getScore(chars, child)));
+
+        }
+
+        return childrenss;
+    }
+
+    private char[] mutateChild(char[] child) {
+        
+        for (int i = 0; i< 3; i++) {
+            int rndChar = rnd.nextInt(child.length);
+            
+            child[rndChar] = randomChar();
+            
+        }
+        
+        return child;
     }
 
     class ScoreComparator implements Comparator<StringAndScore> {
-    @Override
-    public int compare(StringAndScore a, StringAndScore b) {
-        return a.score < b.score ? -1 : a.score == b.score ? 0 : 1;
+
+        @Override
+        public int compare(StringAndScore a, StringAndScore b) {
+            return a.score < b.score ? -1 : a.score == b.score ? 0 : 1;
+        }
     }
-}
-    
+
     public char[] generateRandomCharArray(int length) {
 
         char[] parent = new char[length];
@@ -60,6 +120,10 @@ Collections.sort(scores, new ScoreComparator());
             parent[i] = (char) (rnd.nextInt(127) + 32);
         }
         return parent;
+    }
+
+    private char randomChar() {
+        return (char) (rnd.nextInt(127) + 32);
     }
 
     private ArrayList<char[]> createInitialParents(int num, int targetLength) {
@@ -74,7 +138,7 @@ Collections.sort(scores, new ScoreComparator());
 
     }
 
-    private int getScore(char[] target, char[] attempt) {
+    public int getScore(char[] target, char[] attempt) {
         int score = 0;
         for (int i = 0; i < target.length; i++) {
             int s = (int) (target[i] - attempt[i]);
