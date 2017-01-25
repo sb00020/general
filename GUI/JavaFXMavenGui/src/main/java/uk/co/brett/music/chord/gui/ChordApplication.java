8d5b82203729fe2009acba494ed02e03620e65e4
sb00020 <Sam@Sams-MacBook-Pro.local>
2017-01-25 19:59:11 +0000

    More gui

--- GUI/JavaFXMavenGui/src/main/java/uk/co/brett/music/chord/gui/ChordApplication.java
+++ GUI/JavaFXMavenGui/src/main/java/uk/co/brett/music/chord/gui/ChordApplication.java
@@ -1,6 +1,7 @@
 package uk.co.brett.music.chord.gui;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,15 +10,48 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /** Main application class for Chord Demo fxml demo application */
-public class ChordApplication extends Application {
-  public static void main(String[] args) { launch(args); }
-  @Override public void start(Stage stage) throws IOException {
-    stage.setTitle("Chords");
-   
-    AnchorPane layout = FXMLLoader.load(
-      new URL(ChordApplication.class.getResource("ChordDemo.fxml").toExternalForm())
-    );
-    stage.setScene(new Scene(layout));
-    stage.show();
-  }
-}
+//public class ChordApplication extends Application {
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
+//
+//	@Override
+//	public void start(Stage stage) throws IOException {
+//
+//		//singleSolution(stage);
+//
+//		//masterSolution(stage);
+//		
+//		questionSolution(stage);
+//
+//	}
+//
+//	private void questionSolution(Stage stage) throws MalformedURLException, IOException {
+//		stage.setTitle("Chords");
+//
+//		AnchorPane layout = FXMLLoader
+//				.load(new URL(ChordApplication.class.getResource("ChordDemo2.fxml").toExternalForm()));
+//		stage.setScene(new Scene(layout));
+//		stage.show();
+//		
+//	}
+//
+//	private void masterSolution(Stage stage) throws MalformedURLException, IOException {
+//		stage.setTitle("Chords");
+//
+//		AnchorPane layout = FXMLLoader
+//				.load(new URL(ChordApplication.class.getResource("ChordDemo2.fxml").toExternalForm()));
+//		stage.setScene(new Scene(layout));
+//		stage.show();
+//	}
+//
+//	private void singleSolution(Stage stage) throws MalformedURLException, IOException {
+//		stage.setTitle("Chords");
+//
+//		AnchorPane layout = FXMLLoader
+//				.load(new URL(ChordApplication.class.getResource("ChordDemo.fxml").toExternalForm()));
+//		stage.setScene(new Scene(layout));
+//		stage.show();
+//
+//	}
+//}
