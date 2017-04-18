0fac5874797c2e8f8e92b5a222a5c526260fe47b
Sam Brett <sb00020@googlemail.com>
2017-04-18 05:45:45 +0100

    More geometry

--- Working/Utils/src/test/java/uk/co/generic/TranslationTest.java
+++ Working/Utils/src/test/java/uk/co/generic/TranslationTest.java
@@ -12,15 +12,38 @@ public class TranslationTest {
 		Translation t = new Translation();
 		LatLon l1 = new LatLon(51.0d, 5.0d, 0.0d);
 		LatLon l2 = new LatLon(51.0d, 5.0d, 1000.0d);
-		
+
 		Cartesian c1 = Cartesian.createCartesian(l1);
 		Cartesian c2 = Cartesian.createCartesian(l2);
-		
+
+		// System.out.println(c1);
+		// System.out.println(c2);
+
+		Cartesian c3 = t.translate(c1, c2);
+
 //		System.out.println(c1);
 //		System.out.println(c2);
+//		System.out.println(c3);
 
-		t.translate(c1, c2);
-		
+	}
+
+	@Test
+	public void testNondirect() {
+		Translation t = new Translation();
+		// Buck Palace
+		LatLon l1 = new LatLon(51.5014d, -0.1419d, 0.0d);
+		// Top of Neslons Column
+		LatLon l2 = new LatLon(51.5078d, -0.1279d, 52.0d);
+
+		Cartesian c1 = Cartesian.createCartesian(l1);
+		Cartesian c2 = Cartesian.createCartesian(l2);
+
+		Cartesian c3 = t.translate(c1, c2);
+
+//		System.out.println(c1);
+//		System.out.println(c2);
+//		System.out.println(c3);
+
 	}
 
 }
